52e55c9ee8212b679639313629629295d9f8ce14
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-02 18:10:12 +0600

    add another section and update newsletter

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -16,7 +16,13 @@ const Home = () => {
                     <ArticleCard />
                     <ArticleCard />
                 </div>
-                    <NewsLetter />
+                <NewsLetter />
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    <ArticleCard />
+                    <ArticleCard />
+                    <ArticleCard />
+                </div>
+
             </div>
         </div>
     );
